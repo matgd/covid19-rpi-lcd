339d4387a3602393b81d0babe9c7008ff167927c
matgd <matgd@users.noreply.github.com>
2020-06-21 13:35:30 +0200

    Fixes

--- data_extractor.py
+++ data_extractor.py
@@ -6,14 +6,13 @@ def get_cases():
     page = requests.get('https://www.worldometers.info/coronavirus/')
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    total_table_row = [table_data.get_text() for table_data in soup.find(class_='total_row').find_all_next('td')]
-
-    total_name, total_cases, total_new, _, _, total_recovered, total_active = total_table_row[0:7]
+    total_table_row = [table_data.get_text() for table_data in soup.find('td', string='World').parent.findChildren('td')]
+    total_name, total_cases, total_new, _, _, total_recovered, _, total_active = total_table_row[1:9]
 
     poland_table_row_tags = soup.find(lambda tag: tag.name in ('td', 'a') and 'Poland' in tag.text).parent.find_all('td')
     poland_table_row = [table_data.get_text() for table_data in poland_table_row_tags]
 
-    poland_name, poland_cases, poland_new, _, _, poland_recovered, poland_active = poland_table_row[0:7]
+    poland_name, poland_cases, poland_new, _, _, poland_recovered, _, poland_active = poland_table_row[1:9]
 
     return {
         'World': {
@@ -31,3 +30,9 @@ def get_cases():
             'active': poland_active
         }
     }
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    pprint(get_cases())
--- main.py
+++ main.py
@@ -41,8 +41,10 @@ def prepare_progress_bar_chars(lcd):
         lcd.create_char(3, BAR_3_5)
         lcd.create_char(4, BAR_4_5)
         lcd.create_char(5, BAR_5_5)
-        lcd.create_char(6, BAR_END_5)
+        lcd.create_char(6, BAR_END_0_5)
 
+def assign_chart_to_lcd(lcd, position, char):
+    lcd.create_char(position, char)
 
 lcd.clear()
 lcd.write_string('Getting data...')
@@ -64,18 +66,55 @@ poland_cases_len = len(cases["Poland"]["cases"])
 
 refresh_seconds_left = SECONDS_TO_REFRESH_DATA
 
-def number_to_progress_bar(number: float, distinct_end=True):
+def number_to_progress_bar(lcd, number: float, distinct_end=True):
+    FULL_BLOCK_ID = 0
+    LEFTOVER_BLOCK_ID = 1
+    EMPTY_BLOCK_ID = 2
+    LAST_BLOCK_ID = 3
+
+    LEFTOVER_BLOCK = {
+        0: BAR_0_5,
+        1: BAR_1_5,
+        2: BAR_2_5,
+        3: BAR_3_5,
+        4: BAR_4_5
+    }
+
+    LAST_BLOCK = {
+        0: BAR_END_0_5,
+        1: BAR_END_1_5,
+        2: BAR_END_2_5,
+        3: BAR_END_3_5,
+        4: BAR_END_4_5,
+        5: BAR_END_5_5
+    }
+
     percent = 100 * number
     
     # 1.25 because 16 chars with width of 5 = 80; 100/80 = 1.25
     thin_bars = int(percent // 1.25)
     char_bars = thin_bars // 5
-    progress_bar = [chr(5) for _ in range(char_bars)]
-    progress_bar.append(chr(thin_bars - char_bars * 5))
+
+    assign_chart_to_lcd(lcd, FULL_BLOCK_ID, BAR_5_5)
+    progress_bar = [chr(FULL_BLOCK_ID) for _ in range(char_bars)]
+
+    leftover_bar_length = thin_bars - char_bars * 5
+    
+    assign_chart_to_lcd(lcd, LEFTOVER_BLOCK_ID, LEFTOVER_BLOCK[leftover_bar_length])
+    progress_bar.append(chr(LEFTOVER_BLOCK_ID))
+    
+    assign_chart_to_lcd(lcd, EMPTY_BLOCK_ID, BAR_0_5)
     for i in range(LCD_CHAR_WIDTH - len(progress_bar) - int(distinct_end)):
-        progress_bar.append(chr(0))
-    if distinct_end:
-        progress_bar.append(chr(6))
+        progress_bar.append(chr(EMPTY_BLOCK_ID))
+
+    if distinct_end and percent < 100:
+        if len(progress_bar) < LCD_CHAR_WIDTH:
+            assign_chart_to_lcd(lcd, LAST_BLOCK_ID, LAST_BLOCK[0])
+            progress_bar.append(chr(LAST_BLOCK_ID))
+        else: # last block is already filled
+            assign_chart_to_lcd(lcd, LAST_BLOCK_ID, LAST_BLOCK[leftover_bar_length])
+            progress_bar[-1] = chr(LAST_BLOCK_ID)
+
     return ''.join(progress_bar)
 
 
@@ -91,7 +130,7 @@ while True:
         except AttributeError:
             pass
         refresh_seconds_left = SECONDS_TO_REFRESH_DATA
-    
+    lcd.clear()
     TOTAL_SYMBOL, NEW_SYMBOL = get_stat_chars(lcd)
     # rjust(11) because after ': ' there are only 9 char spaces left
     lcd.write_string(f'WLD {TOTAL_SYMBOL}: {cases["World"]["cases"].rjust(9)}')
@@ -116,8 +155,7 @@ while True:
 
     lcd.write_string('Control:   {:.1%}'.format(closed_cases_fraction))
     lcd.cursor_pos = (1,0)
-    prepare_progress_bar_chars(lcd)
-    lcd.write_string(number_to_progress_bar(closed_cases_fraction))
+    lcd.write_string(number_to_progress_bar(lcd, closed_cases_fraction))
     lcd.cursor_pos = (0,0)
     sleep(SLEEP_SECONDS)
     refresh_seconds_left -= SLEEP_SECONDS
